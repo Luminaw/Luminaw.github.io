5fde2297175c2ff9594de1ee7477d2f4796c9924
Luminaw <146209657+Luminaw@users.noreply.github.com>
2026-01-27 18:28:16 -0600

    feat: add site icons, Open Graph image, and robots.txt for SEO.

--- src/pages/robots.txt.ts
+++ src/pages/robots.txt.ts
@@ -8,7 +8,7 @@ Sitemap: ${new URL('sitemap-index.xml', import.meta.env.SITE).href}
 `.trim();
 
 export const GET: APIRoute = () => {
-    return new ErrorResponse(robotsTxt, {
+    return ErrorResponse(robotsTxt, {
         headers: {
             'Content-Type': 'text/plain; charset=utf-8',
         },
